ae12b80286a59b00e6446908368ce1adf0eded5e
RobsonPeacock <robson_peacock20@outlook.com>
2024-05-29 12:06:55 +0100

    Associate todos with list

---
+++ db/migrate/20240529105848_add_todos_to_lists.rb
@@ -0,0 +1,5 @@
+class AddTodosToLists < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :todos, :list, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_132044) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_105848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_132044) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_todos_on_list_id"
   end
 
+  add_foreign_key "todos", "lists"
 end
